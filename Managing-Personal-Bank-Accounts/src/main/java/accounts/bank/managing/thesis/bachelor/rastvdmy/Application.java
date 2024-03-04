5cb26fc98a6f64893c7131f2777764f4161a4e8b
Unknown <crystalsoldier488@gmail.com>
2024-03-04 22:47:02 +0100

    Adding project to the repo

---
+++ Managing-Personal-Bank-Accounts/src/main/java/accounts/bank/managing/thesis/bachelor/rastvdmy/Application.java
@@ -0,0 +1,13 @@
+package accounts.bank.managing.thesis.bachelor.rastvdmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ Managing-Personal-Bank-Accounts/src/test/java/accounts/bank/managing/thesis/bachelor/rastvdmy/ManagingPersonalBankAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package accounts.bank.managing.thesis.bachelor.rastvdmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagingPersonalBankAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
